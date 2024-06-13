8cc2a6696e0425597e6078534a271d701831584e
Chace Daniels <chace.daniels@outsystems.com>
2024-06-13 00:46:16 -0700

    feat(android): implement openWithSystemBrowser with current customizations

--- src/android/OSInAppBrowser.kt
+++ src/android/OSInAppBrowser.kt
@@ -3,13 +3,18 @@ package com.outsystems.plugins.inappbrowser.osinappbrowser
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import com.outsystems.plugins.inappbrowser.osinappbrowserlib.OSIABEngine
-import com.outsystems.plugins.inappbrowser.osinappbrowserlib.routeradapters.ApplicationContextAdapter
-import com.outsystems.plugins.inappbrowser.osinappbrowserlib.routeradapters.OSIABApplicationRouterAdapter
+import com.outsystems.plugins.inappbrowser.osinappbrowserlib.models.OSIABAnimation
+import com.outsystems.plugins.inappbrowser.osinappbrowserlib.models.OSIABBottomSheet
+import com.outsystems.plugins.inappbrowser.osinappbrowserlib.models.OSIABCustomTabsOptions
+import com.outsystems.plugins.inappbrowser.osinappbrowserlib.models.OSIABViewStyle
+import com.outsystems.plugins.inappbrowser.osinappbrowserlib.routeradapters.OSIABCustomTabsRouterAdapter
+import com.outsystems.plugins.inappbrowser.osinappbrowserlib.routeradapters.OSIABExternalBrowserRouterAdapter
 import com.outsystems.plugins.oscordova.CordovaImplementation
 import org.apache.cordova.CallbackContext
 import org.apache.cordova.CordovaInterface
 import org.apache.cordova.CordovaWebView
 import org.json.JSONArray
+import org.json.JSONException
 
 class OSInAppBrowser: CordovaImplementation() {
     override var callbackContext: CallbackContext? = null
@@ -18,9 +23,9 @@ class OSInAppBrowser: CordovaImplementation() {
 
     override fun initialize(cordova: CordovaInterface, webView: CordovaWebView) {
         super.initialize(cordova, webView)
-        val applicationDelegate = ApplicationContextAdapter(cordova.context)
-        val router = OSIABApplicationRouterAdapter(applicationDelegate)
-        this.engine = OSIABEngine(router)
+        val externalBrowserRouter = OSIABExternalBrowserRouterAdapter(cordova.context)
+        val customTabsRouter = OSIABCustomTabsRouterAdapter(cordova.context)
+        engine = OSIABEngine(externalBrowserRouter, customTabsRouter)
     }
 
     override fun execute(
@@ -33,6 +38,9 @@ class OSInAppBrowser: CordovaImplementation() {
         when(action) {
             "openInExternalBrowser" -> {
                 openInExternalBrowser(args)
+            }
+            "openInSystemBrowser" -> {
+                openInSystemBrowser(args)
             }
         }
 
@@ -51,6 +59,99 @@ class OSInAppBrowser: CordovaImplementation() {
                     sendPluginResult(
                         null,
                         OSInAppBrowserError.OPEN_EXTERNAL_BROWSER_FAILED.toPair()
+                    )
+                }
+            }
+        }
+        catch (e: Exception) {
+            sendPluginResult(null, OSInAppBrowserError.INPUT_ARGUMENTS_ISSUE.toPair())
+        }
+    }
+
+    private fun openInSystemBrowser(args: JSONArray) {
+        try {
+            val argumentsDictionary = args.getJSONObject(0)
+            val url = argumentsDictionary.getString("url")
+
+            val optionsJson = try {
+                argumentsDictionary.getJSONObject("options").getJSONObject("android")
+            } catch (e: JSONException) {
+                null
+            }
+
+            val options = optionsJson?.let {
+                val showTitle = try {
+                    it.getBoolean("showTitle")
+                } catch(e: JSONException) {
+                    true
+                }
+
+                val hideToolbarOnScroll = try {
+                    it.getBoolean("hideToolbarOnScroll")
+                } catch(e: JSONException) {
+                    false
+                }
+
+                val viewStyle = try {
+                    it.getInt("viewStyle").let { ordinal ->
+                        OSIABViewStyle.entries[ordinal]
+                    }
+                } catch(e: JSONException) {
+                    OSIABViewStyle.FULL_SCREEN
+                }
+
+                val bottomSheetOptions = try {
+                    it.getJSONObject("bottomSheetOptions").let { json ->
+                        val height = try {
+                            json.getInt("height")
+                        } catch(e: JSONException) {
+                            1
+                        }
+                        val isFixed = try {
+                            json.getBoolean("isFixed")
+                        } catch(e: JSONException) {
+                            false
+                        }
+
+                        OSIABBottomSheet(height, isFixed)
+                    }
+                } catch(e: JSONException) {
+                    null
+                }
+
+                val startAnimation = try {
+                    it.getInt("startAnimation").let { ordinal ->
+                        OSIABAnimation.entries[ordinal]
+                    }
+                } catch(e: JSONException) {
+                    OSIABAnimation.FADE_IN
+                }
+
+                val exitAnimation = try {
+                    it.getInt("exitAnimation").let { ordinal ->
+                        OSIABAnimation.entries[ordinal]
+                    }
+                } catch(e: JSONException) {
+                    OSIABAnimation.FADE_OUT
+                }
+
+                OSIABCustomTabsOptions(
+                    showTitle = showTitle,
+                    hideToolbarOnScroll = hideToolbarOnScroll,
+                    viewStyle = viewStyle,
+                    bottomSheetOptions = bottomSheetOptions,
+                    startAnimation = startAnimation,
+                    exitAnimation = exitAnimation
+                )
+            }
+
+            engine?.openCustomTabs(url, options) { success ->
+                if (success) {
+                    sendPluginResult("success", null)
+                } else {
+                    sendPluginResult(
+                        null,
+                        OSInAppBrowserError.OPEN_SYSTEM_BROWSER_FAILED.toPair()
                     )
                 }
             }
--- src/android/OSInAppBrowserError.kt
+++ src/android/OSInAppBrowserError.kt
@@ -3,6 +3,7 @@ package com.outsystems.plugins.inappbrowser.osinappbrowser
 enum class OSInAppBrowserError(val code: Int, val message: String) {
     INPUT_ARGUMENTS_ISSUE(0, "The input parameters for 'openInExternalBrowser' are invalid."),
     OPEN_EXTERNAL_BROWSER_FAILED(0, "Couldn't open url using the external browser."),
+    OPEN_SYSTEM_BROWSER_FAILED(0, "Couldn't open url using the system browser."),
 
     GOOGLE_SERVICES_RESOLVABLE_ERROR (200, "Google Play services aren't available on your device."),
     GOOGLE_SERVICES_ERROR (201, "Google Play Services error.");
