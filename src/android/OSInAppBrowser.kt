98b2c8511fa8cabbd819885855a0b68de54fffc1
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2024-06-17 15:17:32 +0100

    feat: implement options for WebView
    References: https://outsystemsrd.atlassian.net/browse/RMET-3426

--- src/android/OSInAppBrowser.kt
+++ src/android/OSInAppBrowser.kt
@@ -1,8 +1,12 @@
 package com.outsystems.plugins.inappbrowser.osinappbrowser
 
+import android.content.Intent
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
+import com.google.gson.Gson
 import com.outsystems.plugins.inappbrowser.osinappbrowserlib.OSIABEngine
+import com.outsystems.plugins.inappbrowser.osinappbrowserlib.models.OSIABToolbarPosition
+import com.outsystems.plugins.inappbrowser.osinappbrowserlib.models.OSIABWebViewOptions
 import com.outsystems.plugins.inappbrowser.osinappbrowserlib.routeradapters.ApplicationContextAdapter
 import com.outsystems.plugins.inappbrowser.osinappbrowserlib.routeradapters.OSIABApplicationRouterAdapter
 import com.outsystems.plugins.oscordova.CordovaImplementation
@@ -10,11 +14,12 @@ import org.apache.cordova.CallbackContext
 import org.apache.cordova.CordovaInterface
 import org.apache.cordova.CordovaWebView
 import org.json.JSONArray
+import org.json.JSONObject
 
 class OSInAppBrowser: CordovaImplementation() {
     override var callbackContext: CallbackContext? = null
-
     private var engine: OSIABEngine? = null
+    private val gson by lazy { Gson() }
 
     override fun initialize(cordova: CordovaInterface, webView: CordovaWebView) {
         super.initialize(cordova, webView)
@@ -34,11 +39,18 @@ class OSInAppBrowser: CordovaImplementation() {
             "openInExternalBrowser" -> {
                 openInExternalBrowser(args)
             }
+            "openInWebView" -> {
+                openInWebView(args)
+            }
         }
 
         return true
     }
 
+    /**
+     * Calls the openExternalBrowser method of OSIABEngine to open the url in the device's browser app
+     * @param args JSONArray that contains the parameters to parse (e.g. url to open)
+     */
     private fun openInExternalBrowser(args: JSONArray) {
         try {
             val argumentsDictionary = args.getJSONObject(0)
@@ -51,6 +63,42 @@ class OSInAppBrowser: CordovaImplementation() {
                     sendPluginResult(
                         null,
                         OSInAppBrowserError.OPEN_EXTERNAL_BROWSER_FAILED.toPair()
+                    )
+                }
+            }
+        }
+        catch (e: Exception) {
+            sendPluginResult(null, OSInAppBrowserError.INPUT_ARGUMENTS_ISSUE.toPair())
+        }
+    }
+
+    /**
+     * Calls the openWebView method of OSIABEngine to open the url in a WebView
+     * @param args JSONArray that contains the parameters to parse (e.g. url to open)
+     */
+    private fun openInWebView(args: JSONArray) {
+        try {
+            val arguments = args.getJSONObject(0)
+            val url = arguments.getString("url")
+
+            val options = arguments.getString("options")
+            val webViewOptions = buildWebViewOptions(options)
+
+            if (webViewOptions == null) {
+                sendPluginResult(null, OSInAppBrowserError.INPUT_ARGUMENTS_ISSUE.toPair())
+                return
+            }
+
+            // so that onActivityResult is called
+            setAsActivityResultCallback()
+
+            engine?.openWebView(this.getActivity(), url, webViewOptions) { success ->
+                if (success) {
+                    sendPluginResult("success", null)
+                } else {
+                    sendPluginResult(
+                        null,
+                        OSInAppBrowserError.OPEN_WEB_VIEW_FAILED.toPair()
                     )
                 }
             }
@@ -80,4 +128,28 @@ class OSInAppBrowser: CordovaImplementation() {
         }
         return true
     }
+
+    private fun buildWebViewOptions(options: String): OSIABWebViewOptions? {
+        return try {
+            gson.fromJson(options, OSInAppBrowserWebViewInputArguments::class.java).let {
+                OSIABWebViewOptions(
+                    it.showURL ?: true,
+                    it.showToolbar ?: true,
+                    it.clearCache ?: true,
+                    it.clearSessionCache ?: true,
+                    it.mediaPlaybackRequiresUserAction ?: false,
+                    it.closeButtonText,
+                    it.toolbarPosition ?: OSIABToolbarPosition.TOP,
+                    it.leftToRight ?: false,
+                    it.showNavigationButtons ?: true,
+                    it.android.allowZoom ?: true,
+                    it.android.hardwareBack ?: true,
+                    it.android.pauseMedia ?: true
+                )
+            }
+        } catch (e: Exception) {
+            return null
+        }
+    }
+
 }
