e7f12bd7b963b3b08dbb80fcb968953e551324f5
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2024-07-10 18:46:52 +0100

    refactor: update error messages
    References: https://outsystemsrd.atlassian.net/browse/RMET-3466

--- src/android/OSInAppBrowserError.kt
+++ src/android/OSInAppBrowserError.kt
@@ -4,8 +4,8 @@ enum class OSInAppBrowserError(val code: Int, val message: String) {
     INPUT_ARGUMENTS_ISSUE(100, "The input parameters for 'openInExternalBrowser' are invalid."),
     INPUT_ARGUMENTS_SYSTEM_BROWSER_ISSUE(101, "The input parameters for 'openInSystemBrowser' are invalid."),
     INPUT_ARGUMENTS_WEB_VIEW_ISSUE(102, "The input parameters for 'openInWebView' are invalid."),
-    OPEN_EXTERNAL_BROWSER_FAILED(103, "Couldn't open {url} using the external browser."),
-    OPEN_SYSTEM_BROWSER_FAILED(104, "Couldn't open {url} using the system browser."),
+    OPEN_EXTERNAL_BROWSER_FAILED(103, "Couldn't open {url} using the device's browser."),
+    OPEN_SYSTEM_BROWSER_FAILED(104, "Couldn't open {url} using Custom Tabs."),
     OPEN_WEB_VIEW_FAILED(105, "Couldn't open {url} using the WebView.");
 
     companion object {
