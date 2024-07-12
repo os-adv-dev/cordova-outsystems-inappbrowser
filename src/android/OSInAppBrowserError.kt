44b90693a20078b5d68c157c2b25086431de8ca7
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2024-07-12 10:41:03 +0100

    refactor: fix error name
    References: https://outsystemsrd.atlassian.net/browse/RMET-3466

--- src/android/OSInAppBrowserError.kt
+++ src/android/OSInAppBrowserError.kt
@@ -1,7 +1,7 @@
 package com.outsystems.plugins.inappbrowser.osinappbrowser
 
 enum class OSInAppBrowserError(val code: Int, val message: String) {
-    INPUT_ARGUMENTS_ISSUE(100, "The 'openInExternalBrowser' input parameters aren't valid."),
+    INPUT_ARGUMENTS_EXTERNAL_BROWSER_ISSUE(100, "The 'openInExternalBrowser' input parameters aren't valid."),
     INPUT_ARGUMENTS_SYSTEM_BROWSER_ISSUE(101, "The 'openInSystemBrowser' input parameters aren't valid."),
     INPUT_ARGUMENTS_WEB_VIEW_ISSUE(102, "The 'openInWebView' input parameters aren't valid."),
     OPEN_EXTERNAL_BROWSER_FAILED(103, "External browser couldn't open the following URL: {url}"),
